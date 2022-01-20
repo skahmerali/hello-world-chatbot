e95cbdc750768374981181c3d3fdd6128fd692a0
SheikhAhmer <sheikhahmerali75@gmail.com>
2022-01-20 13:19:40 +0500

    some code

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Sheikh Ahmer Ali 
+        </p>
+        <p>class : ChatBot </p>
+        <a
+          className="App-link"
+          href="https://github.com/malikasinger1"
+          target="_blank"
+          rel="Teacher"
+        >
+          Learn React From 'SIR INZAMAM MALIK'
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
